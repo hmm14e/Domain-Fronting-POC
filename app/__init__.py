2e3574d5ab09cc4e1a5d9e34337a9fc8c829c490
hannah <hmclaughlin96@yahoo.com>
2018-12-02 16:10:38 -0500

    moving around structure of stuff

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-t_app = Flask(__name__)
-t_app.config.from_object('config')
-
-from app import views
--- run.py
+++
@@ -1,3 +0,0 @@
-#!venv/bin/python
-from app import t_app
-t_app.run(debug=True)
